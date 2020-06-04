7ad9607113f80700ce8b0a481dba37a8f2ceef79
PasqualeAndreola <pasquale.andreola@live.com>
2020-06-04 21:52:20 +0200

    Versione trial di fine lettura dati

--- printname.py
+++
@@ -1,3 +0,0 @@
-name = 'Pasquale'
-age = 23
-print(f'My name is {name} I am {age} yearsold')
--- printnumber.py
+++
@@ -1,5 +0,0 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
-
-number = 10
-print(f, "Il numero scelto è {number}")
